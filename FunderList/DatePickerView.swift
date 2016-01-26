a2fab510665e9cbb37478758d7d69d65a7ccb4ef
Jay <jay.lingelbach@gmail.com>
2016-01-25 18:40:37 -0600

    almost finished

---
+++ FunderList/DatePickerView.swift
@@ -0,0 +1,21 @@
+//
+//  DatePickerView.swift
+//  FunderList
+//
+//  Created by Jay Lingelbach on 1/11/16.
+//  Copyright © 2016 Jay Lingelbach. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FunderList/RepeatView.swift
@@ -0,0 +1,21 @@
+//
+//  RepeatView.swift
+//  FunderList
+//
+//  Created by Jay Lingelbach on 1/18/16.
+//  Copyright © 2016 Jay Lingelbach. All rights reserved.
+//
+
+import UIKit
+
+class RepeatView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
